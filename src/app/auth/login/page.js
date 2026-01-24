cd5df9a26f261d3f6f704be49abafecff7fb59da
karima <karimabenihda@gmail.com>
2026-01-24 16:20:44 +0100

    adjust the google authentification

--- src/app/auth/login/page.js
+++ src/app/auth/login/page.js
@@ -4,6 +4,7 @@ import { Eye, EyeOff } from 'lucide-react'; // Using lucide-react for cleaner ic
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import axios from 'axios';
+import { GoogleLogin, googleLogout, GoogleOAuthProvider } from "@react-oauth/google";
 
 
 const page = () => {
@@ -24,6 +25,24 @@ const page = () => {
       console.log(error)
     }
   }
+
+
+const handleGoogleLogin=async(credentialResponse)=>{
+  try{
+      const tokenId = credentialResponse.credential;
+      const response = await axios.post(
+        "http://127.0.0.1:8000/api/auth/google_login",
+        { token: tokenId }
+      );
+            console.log("JWT from backend:", response.data.access_token);
+      localStorage.setItem("token", response.data.access_token);
+
+    } catch (err) {
+      console.error(err.response?.data || err.message);
+    }
+}
+
+
   const handleChange=(e)=>{
     setFormdata({
       ...formdata,
@@ -87,15 +106,23 @@ const page = () => {
           </div>
 
           {/* Google Login */}
-          <Button className="bg-white border py-2 w-full rounded-xl mt-5 flex justify-center items-center text-sm hover:scale-105 duration-300 hover:bg-[#f0f0f0] font-medium text-gray-600">
+          {/* <Button className="bg-white border py-2 w-full rounded-xl mt-5 flex justify-center items-center text-sm hover:scale-105 duration-300 hover:bg-[#f0f0f0] font-medium text-gray-600">
             <svg className="mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="25px">
               <path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z" />
               <path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z" />
               <path fill="#4CAF50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z" />
               <path fill="#1976D2" d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z" />
-            </svg>
-            Login with Google
-          </Button>
+            </svg> */}
+            {/* Login with Google */}
+
+          <GoogleOAuthProvider clientId="YOUR CLIENT ID">
+              <GoogleLogin
+              onSuccess={handleGoogleLogin}
+              onError={() => console.log("Login Failed")}
+              />     
+          </GoogleOAuthProvider>
+
+          {/* </Button> */}
 
           <div className="mt-2 text-xs border-b border-gray-300 py-4 text-[#1e3753] cursor-pointer">
             Forgot password?
